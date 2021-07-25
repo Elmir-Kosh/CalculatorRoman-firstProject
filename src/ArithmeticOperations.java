58411e2831881c53646d5012350257446ada3b0b
mochaches <50955104+mochaches@users.noreply.github.com>
2021-07-26 01:36:44 +0300

    Update ArithmeticOperations.java

--- src/ArithmeticOperations.java
+++ src/ArithmeticOperations.java
@@ -4,8 +4,8 @@ public class ArithmeticOperations {
 private static double result;
     public static double arithmeticOperation(String firstNumber, String secondNumber, @NotNull String operation)
     {
-    if (operation.equalsIgnoreCase("+")) {
-        result = ConversionOperations.conversionOperation(firstNumber) * 1.0 + ConversionOperations.conversionOperation(secondNumber);
+    if (operation.equalsIgnoreCase("+")) {//todo аналогично, зачем игноркейс для знака?
+        result = ConversionOperations.conversionOperation(firstNumber) * 1.0 + ConversionOperations.conversionOperation(secondNumber); //todo *1.0 для конвертации в дабл?) оригинально, но есть встроенные методы более явные
     }
     else if (operation.equalsIgnoreCase("-")) {
         result = ConversionOperations.conversionOperation(firstNumber) * 1.0 - ConversionOperations.conversionOperation(secondNumber);
